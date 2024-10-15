a3f73cc0c211833c27802db79106e27eeaac5f92
dhruvilrathod <dhruvilrathod2001@gmail.com>
2024-10-15 22:27:39 +1030

    (Feat) Sidenav bottom banner

--- src/app/components/sidebars/bottom-banner/bottom-banner.component.ts
+++ src/app/components/sidebars/bottom-banner/bottom-banner.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 
 @Component({
-  selector: 'admin-bottom-banner',
-  templateUrl: './bottom-banner.component.html',
-  styleUrl: './bottom-banner.component.scss'
+    selector: 'admin-bottom-banner',
+    templateUrl: './bottom-banner.component.html',
+    styleUrl: './bottom-banner.component.scss'
 })
 export class BottomBannerComponent {
+    public bottomBannerVisible = output<boolean>();
 
+    public closeBanner(): void {
+        this.bottomBannerVisible.emit(false);
+    }
 }
